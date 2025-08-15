f2b5e60a37b9b76aa16281f88e4e2a9a3f720bac
Mahabub Tamim <102644237+Tamimbd2@users.noreply.github.com>
2025-08-15 16:02:14 +0600

    Update ArticlesSection.tsx

--- components/ArticlesSection.tsx
+++ components/ArticlesSection.tsx
@@ -14,12 +14,7 @@ const articles: Article[] = [
     tags: ['Tech', 'FlutterApp', 'Firebase' 'EducationalTechnology'],
     url: '#',
   },
-    {
-    imageUrl: 'https://i.postimg.cc/kgsd9Qtj/Gemini-Generated-Image-lsj7a5lsj7a5lsj7.png',
-    title: 'CleanCampus: Revolutionizing University Cleanliness Through Technology',
-    tags: ['Tech', 'FlutterApp', 'Firebase' 'EducationalTechnology'],
-    url: '#',
-  },
+
 
 ];
 
